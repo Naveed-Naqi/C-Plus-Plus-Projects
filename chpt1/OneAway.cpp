a44f12683b9a5f7a7f18bc03509334506e24e7a0
Naveed Naqi <nnaqi5534@gmail.com>
2020-03-04 17:07:21 -0500

    added 2 problems

---
+++ chpt1/OneAway.cpp
@@ -0,0 +1,240 @@
+#include <cmath>
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+/*
+One Away: There are three types of edits that can be performed on strings:
+insert a character, remove a character, or replace a character. Given two
+strings, write a function to check if they are one edit (or zero edits) away.
+*/
+
+/*
+Thought Process:
+
+First things first, edge cases:
+Will we have to deal with any extraneous characters like punctaton or
+whitespace? If we have two strings like this: 'able' and ' able '. They are
+technically two characters away, but should our solution return them as two
+characters away?
+
+It is a good question to ask, just to make sure. For our implementation, let's
+just assume that we don't need to do any type of cleanup with the strigns and
+they will just be single words when they are input into our function.
+
+First Approach: Sorting
+
+Obviously if the length of the two strings differs by greater than 1, we return
+false right away. Otherwise, we can sort the two strings and parse through them
+at the same time and count the differences. If the number of differences exceeds
+one, then we return false. This sounds feasibly but a little complicate to
+implement because we are going to need to account for a bunch of different
+scenarios.
+
+Second Approach: 2 Hash Tables
+Put the two strings into a hashmap to keep track of the characters and number of
+occurences, then compare the two hashmaps. This sounds like it will work and it
+seems less complicated to code than our initial approach. If feels like we can
+still optimize it though.
+
+Third Approach: 1 Hash Table
+After thinking a little more about the problem, using two hashtables seemes
+unnecesary. We can attain all the necessary information by just looking at
+whether the occurence of each character is even or odd. If the strings must
+differ by at most one character, that means that if we hold the sums of each
+character across both strings in a hashtable, there MUST BE a single
+character that has an odd nunber of occurences if the difference of string
+lengths is 1. Otherwise, if the difference in string lengths is 0, then there
+MUST BE 2 letters that have one odd occurence. Let's implement this solution.
+*/
+
+void populateMap(unordered_map<char, int> &map, string arr[], int size) {
+
+  for (int i = 0; i < size; ++i) {
+    for (char &c : arr[i]) {
+      map[c]++;
+    }
+  }
+}
+
+bool oneAwayOneHashTable(string a, string b) {
+  int size_a = a.length();
+  int size_b = b.length();
+  int diff = abs(size_a - size_b);
+
+  if (diff > 1) {
+    return false;
+  }
+
+  unordered_map<char, int> map;
+
+  string arr[2] = {a, b};
+
+  populateMap(map, arr, 2);
+
+  // Seperating this populateMap process into its own separate function is a
+  // nice way of writing code that is cleaner. We could
+  // generalize this functoin more. You can discuss the stylistic
+  // tradeoffs with the interviewer.
+
+  int num_of_odd_occurences = 0;
+
+  for (auto itr : map) {
+    if (itr.second % 2 == 1) {
+      num_of_odd_occurences++;
+    }
+  }
+
+  if (diff == 0) {
+    return (num_of_odd_occurences == 2 || num_of_odd_occurences == 0);
+  } else {
+    return num_of_odd_occurences == 1;
+  }
+}
+
+/*
+We can further optimize the amount of space this function takes up by using a
+bitset instead of hash table. We can do this because we don't actually care
+about the numbere of occurences of each charatcer, we just care about whether
+they are even or odd. This is a nice optimization to bring up in an interview.
+However, it doesn't change the time or space complxity but it is strictly
+better in terms of number of bits that the solution will take up.
+*/
+
+bool oneAwayOneBitVector(string a, string b) {
+  int size_a = a.length();
+  int size_b = b.length();
+  int diff = abs(size_a - size_b);
+
+  if (diff > 1) {
+    return false;
+  }
+
+  bitset<26> map;
+
+  string arr[2] = {a, b};
+
+  for (char &c : a) {
+    size_t pos = (int)c - 97;
+    map.flip(pos);
+  }
+
+  for (char &c : b) {
+    size_t pos = (int)c - 97;
+    map.flip(pos);
+  }
+
+  int num_of_odd_occurences = 0;
+
+  for (int i = 0; i < map.size(); ++i) {
+    if (map.test(i)) {
+      num_of_odd_occurences++;
+    }
+  }
+
+  if (diff == 0) {
+    return (num_of_odd_occurences == 2 || num_of_odd_occurences == 0);
+  } else {
+    return num_of_odd_occurences == 1;
+  }
+}
+
+/*
+Perhaps it is possible to implement a one pass solution, or without an external
+data structure.
+*/
+
+bool oneEditReplace(string a, string b) {
+
+  bool found_diff = false;
+
+  for (int i = 0; i < a.length(); ++i) {
+    if (a[i] != b[i]) {
+      if (found_diff) {
+        return false;
+      }
+      found_diff = true;
+    }
+  }
+
+  return true;
+}
+
+bool oneEditInsert(string smaller, string larger) {
+
+  int index_smaller = 0, index_larger = 0;
+
+  while (index_smaller < smaller.length() && index_larger < larger.length()) {
+    if (smaller[index_smaller] != larger[index_larger]) {
+      if (index_smaller != index_larger) {
+        return false;
+      }
+      index_larger++;
+    } else {
+      index_smaller++;
+      index_larger++;
+    }
+  }
+
+  return true;
+}
+
+bool oneAwayNoDS(string a, string b) {
+  int size_a = a.length();
+  int size_b = b.length();
+  int diff = abs(size_a - size_b);
+
+  if (diff > 1) {
+    return false;
+  }
+
+  if (diff == 0) {
+    return oneEditReplace(a, b);
+  } else if (size_a + 1 == size_b) {
+    return oneEditInsert(a, b);
+  } else {
+    return oneEditInsert(b, a);
+  }
+}
+
+/*
+The logic of oneEditInsert and oneEditReplace are very similar. One thing to
+discuss with an interviewer is the tradeoff between whether or not you should
+merge them into one function or keep them separate. You can argue that it is
+more readable if kept separate, but merging them into one funcion is better from
+a code duplication perspective.
+
+This last implemntation(which was provided by the book), is very similar to the
+initial approach I had with regards to sorting the strings and parsing through
+them. I gave up on that appraoch a little bit too quickly. If I had thought
+about it a little bit moe I would have been able to coem to the same conclusion
+as the book. Also, I overcomplicated it by not truly internalizign and
+understanding the problem. There was no need to sort the strings, it foolishly
+mad my program slower for no reason.
+
+LESSONS:
+Spend a little longer pondering the problem and potential solutions before
+jumping ship to somethign else.
+
+When evaluating a solution, think about whether you really need to do every
+single step of the algorithm andmake sure that every step is neccesary or
+providing value.
+
+A good way to optimize is to try and solve the problem in constant space.
+
+Bitsets are cool and useful for decreasing the number of bits a solution
+takes up, although they don't have any effect on the time or space compelxity of
+a solution.
+*/
+
+int main() {
+
+  cout << oneAwayNoDS("abcwdasda", "abcwdasda"); // 1
+  cout << oneAwayNoDS("abcwdasda", "abcwdsda");  // 1
+  cout << oneAwayNoDS("abcwdasda", "abcwqasda"); // 1
+  cout << oneAwayNoDS("abcwdasda", "abcwdwsda"); // 1
+  cout << oneAwayNoDS("abcwdasda", "abdasda");   // 0
+  cout << oneAwayNoDS("absda", "abcwdasda");     // 0
+
+  return 0;
+}
--- chpt1/PalindromePermutation.cpp
+++ chpt1/PalindromePermutation.cpp
@@ -1,50 +1,46 @@
 #include <iostream>
-#include <unordered_map>
 #include <regex>
+#include <unordered_map>
 using namespace std;
 
-//Given a string, return whether or not it is a permutation of a palindrome.
+// Given a string, return whether or not it is a permutation of a palindrome.
 
 /*
 Thought Process:
 
-The quickest and dirtiest solution is to generate all possible permutations of a string and loop through them all to find one that is a palindrome.
-This is highly inefficient, we have a O(2^n) run time.
+The quickest and dirtiest solution is to generate all possible permutations of a
+string and loop through them all to find one that is a palindrome. This is
+highly inefficient, we have a O(2^n) run time.
 
-Instead we can levarage the definitions of what we are looking for and use them to create a more optimal solution.
-In order for something to be a palindrome, it must have the ability to be mirrored on either side. 
-That means that there must be an even number of all letters, except for a MAX of one. Only one letter is allowed to occur an odd number of times.
+Instead we can levarage the definitions of what we are looking for and use them
+to create a more optimal solution. In order for something to be a palindrome, it
+must have the ability to be mirrored on either side. That means that there must
+be an even number of all letters, except for a MAX of one. Only one letter is
+allowed to occur an odd number of times.
 
 So lets parse through the input string and count the occurences of each number
 */
 
-bool isOdd(const int &num)
-{
-  return num % 2 == 1;
-}
+bool isOdd(const int &num) { return num % 2 == 1; }
 
-bool palindromePermutation(string s)
-{
+bool palindromePermutation(string s) {
   std::unordered_map<char, int> map;
 
-  for (char c : s)
-  {
+  for (char c : s) {
     map[c]++;
   }
 
   int num_of_odd_letters = 0;
 
-  for (auto itr = map.begin(); itr != map.end(); ++itr)
-  {
+  for (auto itr = map.begin(); itr != map.end(); ++itr) {
     std::size_t occurences = itr->second;
 
-    //Talk about the benefits of splitting this up to its own separate function.
-    if (isOdd(occurences))
-    {
+    // Talk about the benefits of splitting this up to its own separate
+    // function.
+    if (isOdd(occurences)) {
       num_of_odd_letters++;
 
-      if (num_of_odd_letters > 1)
-      {
+      if (num_of_odd_letters > 1) {
         return false;
       }
     }
@@ -53,24 +49,24 @@ bool palindromePermutation(string s)
   return true;
 }
 
-//The above solution can be done in one pass by keeping track of the number of odd letters
-//as we populate the map initially and then checking that number at the end. It is implemented below.
+/*
+The above solution can be done in one pass by keeping track of the number of odd
+letters as we populate the map initially and then checking that number at the
+end. It is implemented below. The one pass is not neccesarily more owptimal
+because even though we don't have to parse through the entier hash mpa at the
+end, we sitll have to do an additional operation for each
+*/
 
-bool palindromePermuationOnePass(string s)
-{
+bool palindromePermuationOnePass(string s) {
   std::unordered_map<char, int> map;
   int num_of_odd_letters = 0;
 
-  for (char c : s)
-  {
+  for (char c : s) {
     map[c]++;
 
-    if (isOdd(map[c]))
-    {
+    if (isOdd(map[c])) {
       num_of_odd_letters++;
-    }
-    else
-    {
+    } else {
       num_of_odd_letters--;
     }
   }
@@ -79,42 +75,79 @@ bool palindromePermuationOnePass(string s)
 }
 
 /*
-The above two solutions are assuming that the input string would only be one word and would have no spaces or other punctuation.
-It is a good calrifying question to ask in the beginning of the interview.
-Ask about punctuation/spaces and other possible contents of the string when dealing with these questions. 
+The above two solutions are assuming that the input string would only be one
+word and would have no spaces or other punctuation. It is a good calrifying
+question to ask in the beginning of the interview. Ask about punctuation/spaces
+and other possible contents of the string when dealing with these questions.
 Make sure you know what to return in each case.
 
-If other punctuation and/or spaces can be expected in the string, 
-we can write a quick helper function to clean up the string and only return what we want. We can use Regex.
-If you don't remember exactly how to do it during the interview, this is soe,thing you can just hand wave away. It's not integral to the core
-algorithm, but it shows that you are thinking about all possibilities.
+If other punctuation and/or spaces can be expected in the string,
+we can write a quick helper function to clean up the string and only return what
+we want. We can use Regex. If you don't remember exactly how to do it during the
+interview, this is soe,thing you can just hand wave away. It's not integral to
+the core algorithm, but it shows that you are thinking about all possibilities.
 */
 
-string toLowerCase(string s)
-{
+string toLowerCase(string s) {
 
-  for (char &c : s)
-  {
+  for (char &c : s) {
     c = tolower(c);
   }
 
   return s;
 }
 
-std::string clean(std::string s)
-{
+std::string clean(std::string s) {
   regex not_letters("[^A-Za-z]");
   string result = regex_replace(s, not_letters, "");
   return toLowerCase(result);
 }
 
 /*
-Build up a habit of always asking about edge case inputs when given a question that requires an input.
-It shows that you are thinking outsdie of the box and are willing to addapt and change our solution if given non-perfect conditions.
+Build up a habit of always asking about edge case inputs when given a question
+that requires an input. It shows that you are thinking outsdie of the box and
+are willing to addapt and change our solution if given non-perfect conditions.
 */
 
-int main()
-{
+/*
+The most optimal solution will use a bitset of length 26
+and only keep track of whether the count of each letter is even or odd by
+toggling the element at that index. This solution saves a lot of space because a
+bitset of length 26 only takes up 26 bits, whereas our previous solutions use at
+least one integer to keep track of the count and the integer alone is 4 bytes =
+32 bits.
+*/
+
+bool palindromePermutationBitSet(string s) {
+  bitset<26> map;
+
+  for (char &c : s) {
+    size_t index = (int)c - 97;
+    map.flip(index);
+  }
+
+  size_t counter = 0;
+
+  for (size_t i = 0; i < map.size(); ++i) {
+    if (map.test(i)) {
+      counter++;
+      if (counter > 1) {
+        return false;
+      }
+    }
+  }
+
+  /*
+  It still uses O(N) runtime and there is a more elegant way to check if there
+  is more than a single 1 in the bitvector after you've gone through it, however
+  that requires an intimate knowledge of binary numbers and operators. We might
+  revisit this implementation after we finish Chapter 5.
+  */
+
+  return true;
+}
+
+int main() {
 
   clean("1231sjnalma;aw,dAADs  4165wtgdbA");
 
